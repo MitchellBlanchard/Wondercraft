578e7f9be79b9f991c8ce304fafb5e443d194d10
Mitchell Blanchard <mitchellblanchard3@gmail.com>
2017-03-18 20:02:16 -0400

    Implemented gremlins/goblins. All enemy sprites should now flip properly as well, including the ghosts. Sizes are now passed to enemies to make sure that everything works properly for different sprites

--- Wondercraft/Wondercraft/Enemy.hpp
+++ Wondercraft/Wondercraft/Enemy.hpp
@@ -4,6 +4,8 @@
 #include "Player.hpp"
 #include "RectangleEntity.hpp"
 
+class Model;
+
 namespace EnemyType {
 	enum EnemyType {
 		GOOBER,
@@ -14,11 +16,13 @@ namespace EnemyType {
 
 class Enemy : public RectangleEntity {
 public:
-	Enemy(sf::Vector2f spawn, int, float);
-	void updatePosition(float dt, Player* p);
+	Enemy(sf::Vector2f spawn, int, sf::Vector2f);
+	void updatePosition(float dt, Player* p, Model* m);
 
 	float health, damage;
 	EnemyType::EnemyType type;
+
+	bool right;
 
 private:
 	const float TILE_SIZE;
